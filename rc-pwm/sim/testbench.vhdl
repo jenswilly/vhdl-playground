d862ffaa4a685ba146e30e3d64268332d458e0aa
Jens Willy Johannsen <jens@jenswilly.dk>
2025-07-20 21:58:03 +0200

    Add testbench clk

--- rc-pwm/sim/testbench.vhdl
+++ rc-pwm/sim/testbench.vhdl
@@ -11,20 +11,25 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity testbench is
---  Port ( );
 end testbench;
 
-architecture Behavioral of testbench is
+architecture behavioral of testbench is
     component pwm 
         port(
             a, b, c : in std_logic;
             j, k : out std_logic);
     end component;
     
-    signal abc : std_logic_vector(2 downto 0);
-    signal j, k : std_logic;
+    signal clk_10mhz : std_logic := '0';
+    signal clk_finished : std_logic;
+    constant HALF_PERIOD : time := 50 ns; -- Half period for 10 MHz clock
 
 begin
+    clk : process 
+    begin
+        clk_10mhz <= not clk_10mhz after HALF_PERIOD when clk_finished /= '1' else '0';
+    end process;
+
     UUT: comb port map(
         a => abc(2),
         b => abc(1),
@@ -34,14 +39,10 @@ begin
         
     process
     begin
-        abc <= "000";
-        wait for 100 ns;
-        
-        abc <= "001";
-        wait for 100 ns;
-        
-        abc <= "010";
-        wait;
+        clk_finished <= '0';
+        -- Do stuff with clk
+        -- ...
+        clk_finished <= '1';
     end process;
 
-end Behavioral;
+end behavioral;
