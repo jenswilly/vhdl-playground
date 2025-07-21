6876411a370633040a15b8ee6e6b4043bcf4bed0
Jens Willy Johannsen <jens@jenswilly.dk>
2025-07-21 22:00:57 +0200

    Also fixes

--- rc-pwm/init.tcl
+++ rc-pwm/init.tcl
@@ -4,6 +4,7 @@ create_project "rc-pwm" [pwd] -part "xc7s50csga324-1" -force
 # Add source files
 puts "--- Adding Design Sources ---\n"
 add_files -fileset sources_1 ./src/pwm.vhdl
+set_property FILE_TYPE {VHDL 2008} [get_files ./src/pwm.vhdl]
 
 # Add constraint files
 puts "--- Adding Constraint Files ---\n"
@@ -12,5 +13,7 @@ add_files -fileset constrs_1 ./constraints/arty_s7.xdc
 # Add simulation files
 puts "--- Adding Simulation Sources ---\n"
 add_files -fileset sim_1 ./sim/testbench.vhdl
+set_property FILE_TYPE {VHDL 2008} [get_files ./sim/testbench.vhdl]
+
 
 puts "Project setup complete. Open project by running './open.sh'"
