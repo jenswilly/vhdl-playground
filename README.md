7973ef4b35aa1ece73728e5e9118e197bc69b5ec
Jens Willy Johannsen <jens@jwco.dk>
2025-10-08 21:53:50 +0200

    Script clean-up

--- README.md
+++ README.md
@@ -4,14 +4,14 @@ This project contains several subprojects for testing VHDL in Vivado.
 
 ### Create Project
 
-In subdirectories, first source the Vivado settings script, then run `source init.sh` to create the Vivado project.
+In subdirectories, first source the Vivado settings script, then run `./init.sh` to create the Vivado project.
 
 ### Open Project
 
-If the project's Vivado files have been created, run `source open.sh` to open Vivado and open the project.
+If the project's Vivado files have been created, run `./open.sh` to open Vivado and open the project.
 
 ### Clean
 
-To clean all Vivado-generated files, run `source clean.sh`. After this, it should be possible to re-create 
+To clean all Vivado-generated files, run `./clean.sh`. After this, it should be possible to re-create 
 the project.
 
--- rc-pwm/init.tcl
+++ rc-pwm/init.tcl
@@ -3,20 +3,22 @@ create_project "rc-pwm" [pwd] -part "xc7s50csga324-1" -force
 set_property board_part digilentinc.com:arty-s7-50:part0:1.1 [current_project]
 set_property simulator_language VHDL [current_project]
 
+# Get the directory where this script is located
+set script_dir [file dirname [file normalize [info script]]]
+
 # Add source files
 puts "--- Adding Design Sources ---\n"
-add_files -fileset sources_1 ./src/top.vhdl
-add_files -fileset sources_1 ./src/pwm.vhdl
-set_property FILE_TYPE {VHDL 2008} [get_files ./src/*.vhdl]
+add_files -fileset sources_1 [file join $script_dir src top.vhdl]
+add_files -fileset sources_1 [file join $script_dir src pwm.vhdl]
+set_property FILE_TYPE {VHDL 2008} [get_files [file join $script_dir src *.vhdl]]
 
 # Add constraint files
 puts "--- Adding Constraint Files ---\n"
-add_files -fileset constrs_1 ./constraints/arty_s7.xdc
+add_files -fileset constrs_1 [file join $script_dir constraints arty_s7.xdc]
 
 # Add simulation files
 puts "--- Adding Simulation Sources ---\n"
-add_files -fileset sim_1 ./sim/testbench.vhdl
-set_property FILE_TYPE {VHDL 2008} [get_files ./sim/testbench.vhdl]
-
+add_files -fileset sim_1 [file join $script_dir sim testbench.vhdl]
+set_property FILE_TYPE {VHDL 2008} [get_files [file join $script_dir sim testbench.vhdl]]
 
 puts "Project setup complete. Open project by running './open.sh'"
