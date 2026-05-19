788275f79d1ea9d9158a07ebe1614094ae66e9de
Jens Willy Johannsen <j.johannsen@ext.swissto12.com>
2026-05-19 13:45:02 +0200

    Remove UART drain delay

--- spi-uart-artix7/hdl/core.vhd
+++ spi-uart-artix7/hdl/core.vhd
@@ -16,8 +16,6 @@ entity core is
 end entity core;
 
 architecture rtl of core is
-    constant SPI_IDLE_TIMEOUT_CYCLES : natural := 50_000_000; -- 0.5 s at 100 MHz
-
     -- Debug signals
     signal debug_active : std_logic;
     signal debug_burst_active : std_logic;
@@ -147,30 +145,8 @@ begin
     fifo_in_tvalid <= debug_tvalid or spi_axis_tvalid;
     fifo_in_tdata <= debug_tdata when debug_tvalid = '1' else spi_axis_tdata;
 
-    -- Delay FIFO draining until there has been no SPI data for a while.
-    spi_idle_timer : process (clk)
-    begin
-        if rising_edge(clk) then
-            if rst = '1' then
-                spi_idle_counter <= (others => '0');
-                drain_enable <= '0';
-            else
-                if spi_axis_tvalid = '1' then
-                    spi_idle_counter <= (others => '0');
-                    drain_enable <= '0';
-                elsif drain_enable = '0' then
-                    if spi_idle_counter = to_unsigned(SPI_IDLE_TIMEOUT_CYCLES - 1, spi_idle_counter'length) then
-                        drain_enable <= '1';
-                    else
-                        spi_idle_counter <= spi_idle_counter + 1;
-                    end if;
-                end if;
-            end if;
-        end if;
-    end process spi_idle_timer;
-
     -- Single-cycle start handshake: UART start and FIFO pop happen on the same edge.
-    uart_start_sig <= '1' when fifo_axis_tvalid = '1' and uart_tx_busy_sig = '0' and drain_enable = '1' else '0';
+    uart_start_sig <= '1' when fifo_axis_tvalid = '1' and uart_tx_busy_sig = '0' else '0';
     uart_tx_en_sig <= uart_start_sig;
 
     -- SPI input -> LEDs process
